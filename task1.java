ae6d79398e1bb2246f6e1feabbaaee86a360e6df
oksanaverkh <oksana-tyryatkina@yandex.ru>
2023-02-14 17:21:27 +0300

    Tasks updated

--- task1.java
+++ task1.java
@@ -1,1 +1,47 @@
 // 1) Дана последовательность N целых чисел. Найти сумму простых чисел.
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        // System.out.printf("Sum of simple numbers = " + getSum(0));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter subsequence lenght: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        int sum = 0;
+        System.out.println("Enter numbers: ");
+
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        scanner.close();
+        // System.out.println(Arrays.toString(array));
+        
+        for (int i = 0; i < n; i++) {
+            if (array[i] == 1 || array[i] == 2 || array[i] == 3) {
+                sum += array[i];
+            } else if (array[i] % 2 == 0) {
+                break;
+            } else {
+                for (int j = 3; j < (array[i] / 2 + 1); j = j + 2) {
+                    if (array[i] % j == 0) {
+                        break;
+                    } 
+                sum += array[i];
+                
+                    
+                }
+            }
+
+        }
+        System.out.println(sum);
+
+    }
+
+    // public static int getSum(int sum) {
+
+    // return sum;
+    // }
+}
--- task2.java
+++ task2.java
@@ -1,6 +1,29 @@
-// 2) Дана последовательность из N целых чисел. Верно ли, что последовательность является возрастающей.
+import java.util.Scanner;
 
+public class Task2 {
+    public static void main(String[] args) {
+        if (isIncreasing() == true) {
+            System.out.println("Subsequence is increasing.");
+        } else {
+            System.out.println("Subsequence is NOT increasing.");
+        }
+    }
 
-public class task2 {
-    
+    public static boolean isIncreasing() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number N: ");
+        int n = scanner.nextInt();
+        System.out.println("Enter numbers: ");
+        boolean flag = true;
+        int num1 = scanner.nextInt();
+        for (int i = 1; i < n; i++) {
+            int num2 = scanner.nextInt();
+            if (num1 > num2) {
+                flag = false;
+            }
+            num1 = num2;
+        }
+        scanner.close();
+        return flag;
+    }
 }
